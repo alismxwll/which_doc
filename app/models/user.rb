bc20788df524b03f6771902bfae3c1d3a0266e99
Amy Vaillancourt-Sals & Aoede Pando <student@epicodus.com>
2014-09-10 11:54:45 -0700

    add association for user having many answers

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   validates_uniqueness_of :email
   validates_uniqueness_of :username
   has_many :questions
-
+  has_many :answers
 
   has_secure_password
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -6,5 +6,6 @@ describe User do
   it { should validate_uniqueness_of :email }
   it { should validate_uniqueness_of :username }
   it { should have_many :questions }
+  it { should have_many :answers }
 
 end
