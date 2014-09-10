4bcfb6d62f8dee37c698abc6d40ddda84f819c93
Amy Vaillancourt-Sals & Aoede Pando <student@epicodus.com>
2014-09-10 11:53:44 -0700

    add belongs_to association for answer to user and question

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,4 +2,7 @@ class Answer < ActiveRecord::Base
   validates :question_id, presence: true
   validates :user_id, presence: true
   validates :content, presence: true
+
+  belongs_to :user
+  belongs_to :question
 end
--- spec/answer_spec.rb
+++ spec/answer_spec.rb
@@ -4,5 +4,6 @@ describe Answer do
   it { should validate_presence_of :question_id }
   it { should validate_presence_of :user_id }
   it { should validate_presence_of :content }
-
+  it { should belong_to :user }
+  it { should belong_to :question }
 end
