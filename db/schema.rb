40fcef4a61d3fa23cf85c821b6d956731087d744
Amy Vaillancourt-Sals & Aoede Pando <student@epicodus.com>
2014-09-10 11:10:35 -0700

    add title column to questions table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140910173540) do
+ActiveRecord::Schema.define(version: 20140910180831) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20140910173540) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "title"
   end
 
   create_table "users", force: true do |t|
--- spec/question_spec.rb
+++ spec/question_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 describe Question do
   # it {should belong_to :user}
   it {should validate_presence_of :content}
+  it {should validate_presence_of :title}
 end
