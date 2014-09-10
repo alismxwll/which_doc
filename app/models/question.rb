97bffe28ecefb12f0eb559ebf9aa10423b200e1a
Amy Vaillancourt-Sals & Aoede Pando <student@epicodus.com>
2014-09-10 11:55:36 -0700

    add association for question having many answers

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,5 @@ class Question < ActiveRecord::Base
   belongs_to :user
   validates :content, presence: true
   validates :title, presence: true
+  has_many :answers
 end
--- spec/question_spec.rb
+++ spec/question_spec.rb
@@ -4,4 +4,5 @@ describe Question do
   it {should belong_to :user}
   it {should validate_presence_of :content}
   it {should validate_presence_of :title}
+  it {should have_many :answers}
 end
