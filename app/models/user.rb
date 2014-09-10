d01fce0aea19bf34f77a986f20772a31ccda7bff
Amy Vaillancourt-Sals & Aoede Pando <student@epicodus.com>
2014-09-10 11:47:27 -0700

    add association for user having many questions

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
   validates :username, presence: true
   validates_uniqueness_of :email
   validates_uniqueness_of :username
+  has_many :questions
+
 
   has_secure_password
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -5,5 +5,6 @@ describe User do
   it { should validate_presence_of :username }
   it { should validate_uniqueness_of :email }
   it { should validate_uniqueness_of :username }
+  it { should have_many :questions }
 
 end
