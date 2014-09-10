cd32bdb2f6b8231e13fc2aa8628d2ecdc24ade88
Amy Vaillancourt-Sals & Aoede Pando <student@epicodus.com>
2014-09-10 10:47:42 -0700

    validate content for question

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,5 @@
 class Question < ActiveRecord::Base
+  # belongs_to :user
+  validates :content, presence: true
+
 end
--- spec/question_spec.rb
+++ spec/question_spec.rb
@@ -1,4 +1,6 @@
 require 'rails_helper'
 
 describe Question do
+  # it {should belong_to :user}
+  it {should validate_presence_of :content}
 end
