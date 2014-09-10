3a11b03c820ab3c27a7f47bc734a1150d03488b2
Amy Vaillancourt-Sals & Aoede Pando <student@epicodus.com>
2014-09-10 11:48:03 -0700

    add association for question belonging to user

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ActiveRecord::Base
-  # belongs_to :user
+  belongs_to :user
   validates :content, presence: true
   validates :title, presence: true
 end
--- spec/question_spec.rb
+++ spec/question_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe Question do
-  # it {should belong_to :user}
+  it {should belong_to :user}
   it {should validate_presence_of :content}
   it {should validate_presence_of :title}
 end
