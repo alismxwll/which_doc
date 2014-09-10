e493a08e67214cf4ba40cdb6d850baa2a80bff81
Amy Vaillancourt-Sals & Aoede Pando <student@epicodus.com>
2014-09-10 12:18:50 -0700

    establish CRUD on user class

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,45 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_url, notice: "Thank you for signing up!"
+    else
+      render "new"
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "User Information Updated"
+      redirect_to user_path(@user)
+    else
+      flash[:alert] = "No Bueno."
+      render 'edit'
+    end
+  end
+
+  def delete
+    @user = User.find(params[:id])
+    @user.delete
+      flash[:notice] = "User Deleted"
+      redirect_to users_path
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root :to => 'questions#index'
 
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
   resources :users
   resources :sessions
   resources :questions
