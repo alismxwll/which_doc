1d8176f81c384ba56a10dbec57c9f47634d44e7f
Amy Vaillancourt-Sals & Andrew Akers <student@epicodus.com>
2014-09-16 11:46:44 -0700

    make login method more DRY

--- spec/utilities.rb
+++ spec/utilities.rb
@@ -1,6 +1,6 @@
 def log_in(user)
   visit 'login'
-  fill_in :email, with: "Jane@doe.com"
-  fill_in :password, with: "HarryIsBigfoot"
+  fill_in :email, with: user.email
+  fill_in :password, with: user.password
   click_on "Enter"
 end
